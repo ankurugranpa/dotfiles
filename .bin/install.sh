2788c1dc13b7f0cd0f412a86190c52ada2848006
ahahahaha <torimeriodasu@gmail.com>
2022-03-11 19:05:35 +0900

    update installer

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 #引数の設定
-if [ $# -ne 1 ]; then
-	echo "error:you need to set -arg1"
-	exit 1
-fi
-
-if [ $1 = -arch ]; then
-	SHELL_TYPE=1
-elif [ $1 = -ubuntu ]; then
-	SHELL_TYPE=2
-fi
-
 
 #backupファイルの作成
 if [ -e $HOME/backup ]; then
@@ -40,21 +29,28 @@ do
 done < $HOME/dotfiles/.bin/install_file.txt
 
 #ubuntuの場合ログインシェルを変更する
-if [ $SHELL_TYPE==2 ]; then
-	sudo apt-get install zsh
-	chsh -s $(which zsh)
-fi
+#if [ $SHELL_TYPE==2 ]; then
+#	sudo apt-get install zsh
+#	chsh -s $(which zsh)
+#fi
+
 #denoのインストール
-curl -fsSL https://deno.land/install.sh | sh
+if [ $1 = -deno]; then
+        curl -fsSL https://deno.land/install.sh | sh
+        deno run https://deno.land/std/examples/welcome.ts
+	
+fi
+
+
 
 #zplugのインストール
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
+source $HOME/.zshrc
 zplug install
 
-source ~/.zshrc
 #vim-plugのインストール
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "インストールに成功しました　vim-plugでプラグインを最後にインストールしてください"
+echo "プロセスはすべて終了しました":wq
+
