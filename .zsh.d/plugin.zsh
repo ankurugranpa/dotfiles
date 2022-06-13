341128bd79ee71031730a95bd40b4b5dda503f8e
ahahahaha <torimeriodasu@gmail.com>
2022-06-13 10:28:06 +0900

    [update] .zshrc at plugin
    fzfのインストールに関しての設定を変えた

--- .zsh.d/plugin.zsh
+++ .zsh.d/plugin.zsh
@@ -3,11 +3,6 @@ source ~/.zplug/init.zsh
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions"
-zplug "junegunn/fzf-bin", \
-    from:gh-r, \
-    as:command, \
-    rename-to:fzf, \
-    use:"*darwin*amd64*"
 zplug "b4b4r07/enhancd", use:"init.sh"
 
 if ! zplug check --verbose; then
