dfff3c1bbb1cb6bfc36129b8d3107f54cde55b05
ahahahaha <torimeriodasu@gmail.com>
2022-06-15 19:52:07 +0900

    [upodate] update to open() function
    chromeオプションを完成させた

--- .zsh.d/bin.zsh
+++ .zsh.d/bin.zsh
@@ -20,23 +20,13 @@ function open {
 	if [ $1 = "webclass" ];then
 		action="https://ecsylms1.kj.yamagata-u.ac.jp/webclass/login.php?rnd=b0f03&language=JAPANESE"
 	elif [ $1 = "chrome" ];then
-		"/mnt/c/Program Files/Google/Chrome/Application/chrome.exe"
-		break
+		action="/mnt/c/Program Files/Google/Chrome/Application/chrome.exe"
+		"/mnt/c/Program Files (x86)/VDesk/VDesk.exe" on:$desktop_num run:cmd.exe /C "C:\Program Files\Google\Chrome\Application\chrome.exe"
+		return 0
 	else
 		action=$1
 	fi
 
-	if [ -z $2 ];then
-		desktop_num=${2:=1}
-	else
-		if [[ $1 == *".pdf" ]]
-		then
-			desktop_num=3
-		elif [[ $1 == *".docx" ]]
-		then
-			desktop_num=4
-		fi
-	fi
 	desktop_num=${2:=1}
 
 	"/mnt/c/Program Files (x86)/VDesk/VDesk.exe" on:$desktop_num run:cmd.exe /C start /max $action 
