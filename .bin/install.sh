5f2962ef34412dad2e650d3774befbfa005e6541
ahahahaha <torimeriodasu@gmail.com>
2022-07-31 13:28:53 +0900

    [update] install.sh
    install.shの見た目を少し良くした

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,15 +2,15 @@
 #引数の設定
 
 #backupファイルの作成
-if [ -e $HOME/backup ]; then
-	echo "exist backupfile"
+if [ -e $HOME/.config_backup ]; then
+	echo "[exist] backupfile"
 else
-	echo "making backupfaile"
-	mkdir $HOME/backup
+	echo "[make] backupfaile"
+	mkdir $HOME/.config_backup
 fi
 #バックアップファイル内に作業ファイルを作成する
 #何らかのことがあったときのためファイルは日時で管理する
-backup=$HOME/backup/$(date +%Y%m%d%H%W)
+backup=$HOME/.config_backup/$(date +%Y%m%d%H%W)
 mkdir $backup
 #install_file.txtファイル内に記述されたファイルの存在確認
 #ファイルが存在した場合は場はbackupに格納する
@@ -19,11 +19,11 @@ while read line
 do
 	FILE=$HOME/$line
         if [ -e $FILE ]; then
-		echo "$lineは存在しました"
+		echo "[exist]$line"
 		mv $FILE $backup/$line
 	        ln -s $HOME/dotfiles/$line $FILE		
         else
-	        echo "$lineは存在しませんでした"	
+	        echo "[no exist]$line"	
 	        ln -s $HOME/dotfiles/$line $FILE
 	fi
 done < $HOME/dotfiles/.bin/install_file.txt
@@ -36,6 +36,7 @@ done < $HOME/dotfiles/.bin/install_file.txt
 
 #denoのインストール
 if [ $1 = -deno]; then
+	echo "[install] deno"
         curl -fsSL https://deno.land/install.sh | sh
         deno run https://deno.land/std/examples/welcome.ts
 	
@@ -43,19 +44,22 @@ fi
 
 # nvimのインストール
 if !(type "nvim" > /dev/null 2>&1); then
+	echo "[install] NeoVim"
 	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 	chmod u+x nvim.appimage
 	mv nvim.appimage ~/dotfiles/mybin/nvim
 fi
 
 #zplugのインストール
+echo "[install] zplug"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 source $HOME/.zshrc
 zplug install
 
 #vim-plugのインストール
+echo "[install] vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "プロセスはすべて終了しました":wq
+echo "Finish!!!"
 
