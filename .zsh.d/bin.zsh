21f0f6b1928ba604e35712543df9d5512dcd05c5
ankuru <yuki.tori.39@gmail.com>
2023-06-22 20:37:14 +0900

    [fix] change open command
    openコマンドをexplorer.exeに変えた

--- .zsh.d/bin.zsh
+++ .zsh.d/bin.zsh
@@ -1,34 +1,20 @@
 function venv {
+	# activate venv 
 	source venv/bin/activate
 }
 autoload -Uz venv
 
 
 function word {
+	# open word(msoffice) 
 	"/mnt/c/Program Files/Microsoft Office/root/Office16/WINWORD.EXE" /t $1 &
 }
 autoload -Uz word
 
-function pdf {
-	pdf_path=`wslpath -w $1`
-	/mnt/c/Program\ Files\ \(x86\)/Microsoft/Edge/Application/msedge.exe $pdf_path
-}
-autoload -Uz pdf
 
 
 function open {
-	if [ $1 = "webclass" ];then
-		action="https://ecsylms1.kj.yamagata-u.ac.jp/webclass/login.php?rnd=b0f03&language=JAPANESE"
-	elif [ $1 = "chrome" ];then
-		action="/mnt/c/Program Files/Google/Chrome/Application/chrome.exe"
-		"/mnt/c/Program Files (x86)/VDesk/VDesk.exe" on:$desktop_num run:cmd.exe /C "C:\Program Files\Google\Chrome\Application\chrome.exe"
-		return 0
-	else
-		action=$1
-	fi
-
-	desktop_num=${2:=1}
-
-	"/mnt/c/Program Files (x86)/VDesk/VDesk.exe" on:$desktop_num run:cmd.exe /C start /max $action 
+	# open something(with default APP) on Windows
+	explorer.exe $1
 }
-autoload -Uz open_beta
+autoload -Uz open
