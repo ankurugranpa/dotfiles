3ea7f45789291b29d3344ac4d4c6ab9f44cc9c6e
ankuruguranpa <yuki.tori.39@gmail.com>
2024-01-07 13:49:02 +0900

    fead: [Add SetUp tools for PowerShell]
    PowerShellでの環境セットアップの自動化ツールができた

--- .installer/setup.ps1
+++ .installer/setup.ps1
@@ -2,10 +2,7 @@
 if((Get-ExecutionPolicy -Scope LocalMachine) -ne "RemoteSigned"){Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force}
 
 # Profileの存在判定
-if(-not (Test-Path $PROFILE)){
-        New-Item $PROFILE -Type File -Force
-}
-else{
+if((Test-Path $PROFILE)){
         $backup_dir = "$HOME/prompt_backup"
         if(-not (Test-Path $backup_dir)){
                 New-Item $backup_dir -ItemType Directory -Force
@@ -14,14 +11,20 @@ else{
         $BuckupFile = "$backup_dir/$(Get-Date -Format 'yyyyMMdd_HHmmss')_backup.txt"
         $ProfileContent = Get-Content $PROFILE  -Raw
         New-Item $BuckupFile
-        Add-Content -Path $BuckupFile -Value $ProfileContent
+	Add-Content -Path $BuckupFile -Value $ProfileContent
+	Remove-Item $PROFILE
 }
+New-Item $PROFILE -Type File -Force
+
 
 # PROFILEの設定
-$fileAPath = "./PROFILE.txt"
+$profile_txt = "../windows/PROFILE.txt"
 
-# ファイルAの内容を読み込む
-$fileAContent = Get-Content -Path $fileAPath -Raw
+$fileAContent = Get-Content -Path $profile_txt -Raw
 
 # $PROFILEに内容を追加
 Add-Content -Path $PROFILE -Value $fileAContent
+
+## Install from scoop
+scoop install busybox
+scoop install fzf
