73318f1b0547b9d47ee165ec20bd30e5a89b8b28
ankuru <ankuru.ahahahaha@gmail.com>
2024-04-05 17:41:00 +0900

    fix: [ path setting for app that installed by snap]

--- .zsh.d/setting.zsh
+++ .zsh.d/setting.zsh
@@ -42,3 +42,9 @@ alias clip.exe='iconv -t sjis | clip.exe'
 
 alias poetry='python3 -m poetry'
 alias pythonp='poetry run python3'
+
+# Expand $PATH to include the directory where snappy applications go.
+snap_bin_path="/snap/bin"
+if [ -n "${PATH##*${snap_bin_path}}" -a -n "${PATH##*${snap_bin_path}:*}" ]; then
+    export PATH=$PATH:${snap_bin_path}
+fi
