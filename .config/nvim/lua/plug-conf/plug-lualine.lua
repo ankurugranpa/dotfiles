a869865e3ef7f42b8a4b377813f1e7fe50118eda
ankuru <ankuru.ahahahaha@gmail.com>
2024-06-10 02:38:43 +0900

    feat: winbarの設定追加(ファイル名,関数のようなUIにした)

--- .config/nvim/lua/plug-conf/plug-lualine.lua
+++ .config/nvim/lua/plug-conf/plug-lualine.lua
@@ -36,7 +36,40 @@ require('lualine').setup {
     lualine_z = {}
   },
   tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+  winbar = {
+	-- lualine_a = {'filename'},
+	lualine_a = {'filetype'},
+        lualine_b = {
+          {
+            "aerial",
+            -- ステータスラインでシンボルを区切るために使用されるセパレーター。
+            sep = " ) ",
+
+            -- 上から下にレンダリングするシンボルの数。最後の'N'個のシンボルのみを
+            -- レンダリングするには、負の数を指定することも可能です。例えば、'depth = -1'
+            -- と設定すると現在のシンボルのみをレンダリングします。
+            depth = nil,
+
+            -- 'dense'モードがオンの場合、アイコンはシンボルの近くにレンダリングされません。
+            -- 現在のシンボルの種類を表す単一のアイコンのみがステータスラインの先頭に
+            -- レンダリングされます。
+            dense = false,
+
+            -- denseモードでシンボルを区切るために使用されるセパレーター。
+            dense_sep = ".",
+
+            -- シンボルアイコンに色を付けます。
+            colored = true,
+          },
+      },
+  },
+  inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+  },
+  extensions = {'toggleterm'},
 }
