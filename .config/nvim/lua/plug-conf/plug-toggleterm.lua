c2b39d4dc3901c812e5ce2c4be2e91176b4f98fa
ankuru <ankuru.ahahahaha@gmail.com>
2024-06-10 01:36:26 +0900

    feat: lazygitをnvimから開けるようにした

--- .config/nvim/lua/plug-conf/plug-toggleterm.lua
+++ .config/nvim/lua/plug-conf/plug-toggleterm.lua
@@ -5,7 +5,7 @@ toggleterm.setup({
 	shade_filetypes = {},
 	shade_terminals = true,
 	shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-	start_in_insert = false,
+	start_in_insert = true,
 	close_on_exit = false,
 	direction = 'float',
 	shell = vim.o.shell,
@@ -13,10 +13,38 @@ toggleterm.setup({
 	loat_opts = {
 		border = 'single',
 		width = math.floor(vim.o.columns * 0.9),
-		eight = math.floor(vim.o.lines * 0.9),
+		height = math.floor(vim.o.lines * 0.9),
 		winblend = 3,
 		highlights = { border = "ColorColumn", background = "ColorColumn" },
 		title_pos = 'center',
   	},
 })
 vim.api.nvim_set_keymap("n", "<C-z>", '<Cmd>execute v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true })
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({
+	open_mapping = [[<c-g>]],
+	start_in_insert = true,
+	count = 5,
+	cmd = "lazygit",
+	dir = "git_dir",
+	direction = "float",
+	float_opts = {
+	border = "double",
+	},
+	-- function to run on opening the terminal
+	on_open = function(term)
+	-- vim.cmd("startinsert!")
+	vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+	end,
+	-- function to run on closing the terminal
+	on_close = function(term)
+	vim.cmd("startinsert!")
+	end,
+})
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<c-g>", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
