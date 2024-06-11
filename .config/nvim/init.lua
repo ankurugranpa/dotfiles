4d7e7bd276a4cbfefdd29df5e42edcea0a0f4da3
ankuru <ankuru.ahahahaha@gmail.com>
2024-06-12 00:49:49 +0900

    feat: code_actionなどの挙動がうっとおしいのでsignの設定をした, tabをspace2文字に変更した

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,9 @@ require('base')
 require('keymaps')
 require('options')
 require('plugins')
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.api.nvim_win_set_option(0, 'signcolumn', 'yes:2')
