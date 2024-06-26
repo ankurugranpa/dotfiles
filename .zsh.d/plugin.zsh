c2564c72decd7409c79d270944ae3348ae5f2742
ankuru <ankuru.ahahahaha@gmail.com>
2024-06-26 13:30:03 +0900

    zplugのインストール画面の省略

--- .zsh.d/plugin.zsh
+++ .zsh.d/plugin.zsh
@@ -6,10 +6,7 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "b4b4r07/enhancd", use:"init.sh"
 
 if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
+  zplug install
 fi
 
 zplug load --verbose
