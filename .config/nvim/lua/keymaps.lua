bb305c55a103a4d2873fa1d0deba4e371b61c6e3
ankuru <ankuru.ahahahaha@gmail.com>
2025-08-30 20:56:40 +0900

    add: serena_mcp command

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,6 +1,10 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Space>", "<Nop>")
+
+
 -- Modes
 --   normal_mode = 'n',
 --   insert_mode = 'i',
