c6b029a90193195a3f27702d87f32cc309fc895b
ankuruguranpa <yuki.tori.39@gmail.com>
2026-03-06 17:23:35 +0900

    fix: gitignore

--- windows/profiles/alias.ps1
+++ windows/profiles/alias.ps1
@@ -1,3 +1,7 @@
 Set-Alias vim nvim # nvimのエイリアス
 
 Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete # 補完の挙動をzshと同じにする
+
+function ls {
+    Get-ChildItem | Select-Object -ExpandProperty Name
+}
--- windows/profiles/prompt.ps1
+++ windows/profiles/prompt.ps1
@@ -1,7 +1,9 @@
 Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit # ctrl-Dの有効化
 
 # oh-my-poshの設定
-oh-my-posh init pwsh --config "$HOME/dotfiles/windows/themes/ys.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$HOME/dotfiles/windows/themes/tokyo.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$HOME/dotfiles/windows/themes/blueish.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$HOME/dotfiles/windows/themes/froczh.omp.json" | Invoke-Expression
 
 # fzf
 function ghq-fzf {
